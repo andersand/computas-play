7ea90e9bfdc27f06551a851f218e0c6acf39ba6f
andersand <anders.sandberg.nordbo@gmail.com>
2013-04-03 16:06:39 +0300

    Moar rotation plz

--- play.js
+++ play.js
@@ -1,6 +1,13 @@
-var play = function(){
-  console.log('Velkommen til konsollet. Skriv play.spin() og se hva som skjer! Skriv play.help() for hjelp og play.about() for bakgrunnsinfo ');
-};
-play.spin = function () { $("body").css("-webkit-transform", "rotate3d(1,1,0,180deg)")};
-play.help = function () { console.log("Skriv play.spin() og trykk enter")};
-play.about = function () { console.log("Sjekk ut http://blog.bjartwolf.com/?p=552") };
+(function() {
+  var play = function() {
+    console.log('Velkommen til konsollet. Skriv play.spin() og se hva som skjer! Skriv play.help() for hjelp og play.about() for bakgrunnsinfo ');
+  };
+  var spin_offset = 0;
+  play.spin = function () {
+    spin_offset = (spin_offset > 3) ? 1 : spin_offset + 1;
+    $("body").css("-webkit-transform", "rotate3d(0,0,1," + (spin_offset * 90) + "deg)");
+  };
+  play.help = function () { console.log("Skriv play.spin() og trykk enter") };
+  play.about = function () { console.log("Sjekk ut http://blog.bjartwolf.com/?p=552") };
+  window.play = play;
+})();
